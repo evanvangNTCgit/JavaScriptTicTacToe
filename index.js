5397107bad3599117d85de969029e736e9fc2d2d
Evan Vang <evankvang@gmail.com>
2025-10-17 09:49:24 -0500

    Added some functionality for these images. Now to add the game logic

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function helloWorld(image){
+let xTurn = false;
+
+let turnHeader = document.querySelector('#whosTurnHeader');
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (xTurn) {
+    turnHeader.textContent = 'X Turn';
+  } else {
+    turnHeader.textContent = 'O Turn';
+  }
+});
+
+function changeImage(image) {
+  if (xTurn) {
     image.src = 'x.png';
+    turnHeader.textContent = 'O Turn';
+    xTurn = false;
+  } else {
+    image.src = 'o-300x300.png';
+    turnHeader.textContent = 'X Turn';
+    xTurn = true;
+  }
 }
