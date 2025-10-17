e305cf2ff5d25823a468930b7100e557288cbcac
Evan Vang <evankvang@gmail.com>
2025-10-17 11:21:45 -0500

    Finally done. Should add some finishing touches like maybe adding a buttont to reset the board instead of automatically doing it

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let xTurn = false;
 let turnHeader = document.querySelector('#whosTurnHeader');
 
-let topRow = [4, 44, 444];
-let middleRow = [4, 44, 444];
-let bottomRow = [4, 44, 444];
+let topRow = [123, 321, 213132];
+let middleRow = [41232131323, 421313212342, 213213132];
+let bottomRow = [41231231321, 442, 44313232121343];
 // 1 = X, 0 = O
 
 document.addEventListener('DOMContentLoaded', () => {
+  const images = document.getElementsByTagName('img');
+  for (let i = 0; i < images.length; i++) {
+    console.log('hi');
+    images[i].src = 'Question-Mark-Transparent.webp';
+  }
+
   if (xTurn) {
     turnHeader.textContent = 'X Turn';
   } else {
@@ -94,7 +100,49 @@ function handleGridUpdate(id) {
       }
       break;
   }
-  checkWinner(topRow, middleRow, bottomRow);
+
+  if (checkWinner(topRow, middleRow, bottomRow)) {
+    window.alert('Winner! \nResetting board.');
+    resetBoard(topRow, middleRow, bottomRow);
+  }
+
+  if (checkDraw(topRow, middleRow, bottomRow)) {
+    window.alert('Draw! \nResetting board.');
+    resetBoard(topRow, middleRow, bottomRow);
+  }
+}
+
+function resetBoard(t, m, b) {
+  topRow = [123, 321, 213132];
+  middleRow = [41232131323, 421313212342, 213213132];
+  bottomRow = [41231231321, 442, 44313232121343];
+
+  const images = document.getElementsByTagName('img');
+  for (let i = 0; i < images.length; i++) {
+    console.log('hi');
+    images[i].src = 'Question-Mark-Transparent.webp';
+  }
+}
+
+function checkDraw(t, m, b) {
+  let sum = 0;
+
+  for (let i = 0; i < t.length; i++) {
+    sum += parseInt(t[i]);
+  }
+  for (let i = 0; i < m.length; i++) {
+    sum += parseInt(m[i]);
+  }
+  for (let i = 0; i < b.length; i++) {
+    sum += parseInt(b[i]);
+  }
+
+  console.log(sum);
+  if (sum > 10) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function checkWinner(t, m, b) {
@@ -103,17 +151,48 @@ function checkWinner(t, m, b) {
    * Then diagonals
    * If any show win return true;
    */
-    function checkRow(array){
-        let checkValue = array[0]
+  function checkHorizontalRow(array) {
+    let checkValue = array[0];
 
-        if(array[1] === checkValue && array[2] === checkValue){
-            window.alert('Winner');
-            return true;
-        }
+    if (array[1] === checkValue && array[2] === checkValue) {
+      //window.alert('winner');
+      return true;
     }
+  }
 
-    checkRow(t);
-    checkRow(m);
-    checkRow(b);
+  if (checkHorizontalRow(t) || checkHorizontalRow(m) || checkHorizontalRow(b)) {
+    return true;
+  }
 
+  function checkVerticalRow(t, m, b) {
+    // Loop through the three verticals.
+    // Get the top vertical value and check the others.
+
+    for (let i = 0; i < 3; i++) {
+      if (t[i] === m[i] && t[i] === b[i]) {
+        //window.alert('winner');
+        return true;
+      }
+    }
+  }
+
+  if (checkVerticalRow(t, m, b)) {
+    return true;
+  }
+
+  function checkDiagonalRows(t, m, b) {
+    if (t[0] === m[1] && t[0] === b[2]) {
+      //window.alert('winner');
+      return true;
+    } else if (t[2] === m[1] && t[2] === b[0]) {
+      //window.alert('winner');
+      return true;
+    }
+  }
+
+  if (checkDiagonalRows(t, m, b)) {
+    return true;
+  }
+
+  return false;
 }
